ff348afac8d0af718cfb9acd4000610ef5a287e8
Michael Linsinbigler <malinsinbigler@gmail.com>
2018-09-30 17:40:49 -0400

    Added the create and show resources for posts. Pulled in bootstrap and active_link_to for styling

--- simpleblog/app/controllers/posts_controller.rb
+++ simpleblog/app/controllers/posts_controller.rb
@@ -1,5 +1,33 @@
 class PostsController < ApplicationController
 	def index
-		
+		@posts = Post.all
+	end
+
+	def new
+
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		# view input parameters via page output
+		# render plain: params[:post].inspect
+
+
+
+		#accept only valid post params
+		@post = Post.new(post_params)
+
+		#save this post to the db
+		@post.save
+
+		#present this new post to the user
+		redirect_to @post
+	end
+
+	private def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
--- simpleblog/config/routes.rb
+++ simpleblog/config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #route for the root
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   #standard get route
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
+
+  #resource route
+  #you can view all endpoints by doing 'rake routes'
+  resources :posts
 
 end
